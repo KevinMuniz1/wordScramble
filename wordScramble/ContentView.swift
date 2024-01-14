4c9f972b1442d9eaaf3526535c624feeaf15aee4
KevinMuniz1 <munizkevin@outlook.com>
2024-01-14 16:36:47 -0500

    Initial Commit

---
+++ wordScramble/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  wordScramble
+//
+//  Created by Kevin Muniz on 1/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Kevin Muniz on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
